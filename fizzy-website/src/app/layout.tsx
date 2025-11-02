f7ae6ea4027c4034b386b165bb2c548b6aa27967
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-11-02 11:47:01 +0500

    inital setup

---
+++ fizzy-website/src/app/layout.tsx
@@ -0,0 +1,24 @@
+// import localFont from "next/font/local";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import "./globals.css";
+
+// const alpino = localFont({
+//   src: '../public/fonts/Alpino.woff2',
+//   display: 'swap',
+//   weight: '100 900',
+//   variable: '--font-alpino',
+// });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
